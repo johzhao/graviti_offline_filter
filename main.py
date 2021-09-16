3c4bb1b7a6d8311566ef508985ddc9b8a70b67c6
John <joh.zhao@gmail.com>
2021-09-16 14:23:26 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+def init(params: dict):
+    # 进行初始化工作，可以返回一个函数或None
+    return None
+
+
+def run(data: dict, func=None) -> bool:
+    # 执行判断操作，data传入的是一个数据集文件相关的数据，func时init返回的函数
+    # 如果通过筛选返回True，否则返回False
+    return True
+
+
+def teardown():
+    # 执行清理工作，没有返回值
+    pass
